0622bf296bcfea8a75a7e4f075460613a7786bf0
RZ Fang <skywine13@gmail.com>
2017-01-13 23:09:49 +0800

    all pages move to node.js

--- SRC/pages.js
+++ SRC/pages.js
@@ -1,14 +1,100 @@
 module.exports = {
+  '/colors': {
+    title: 'Color Conside - Web Tool',
+    keywords: '顏色, 色票, 樣式, color, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'colors.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/convert': {
+    title: 'Convert Table - Web Tool',
+    keywords: '轉換, 換算, 查表, convert, list, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'convert.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/datauri': {
+    title: 'File to Data URI - Web Tool',
+    keywords: '資料, 轉換, datauri, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'datauri.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/datetime': {
+    title: 'Datetime Transform - Web Tool',
+    keywords: '時間格式轉換, datetime transform, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'datetime.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/endecode': {
+    title: 'String Encode/Decode - Web Tool',
+    keywords: '編碼, 解碼, encoding, decoding, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'endecode.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/ipv426': {
+    title: 'IPv4 to v6 convert - Web Tool',
+    keywords: 'ipv6, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'ipv426.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/json': {
+    title: 'JSON Edit - Web Tool',
+    keywords: 'JSON, edit, 編輯, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'json.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/keycode': {
+    title: 'Key Code Detect - Web Tool',
+    keywords: '鍵盤代碼, keyboard, keyCode, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'keycode.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/post': {
+    title: 'Form Post - Web Tool',
+    keywords: '送出資料, form, post, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'post.part.html',
+      'footer.part.html'
+    ]
+  },
   '/re': {
     title: 'Regula Expression - Web Tool',
     keywords: '正規表示式, RegExp, regular expression, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
-    js: [
-      '/resource/z.min.js',
-      'https://cdn.jsdelivr.net/riot/3.0/riot.min.js'
-    ],
     body: [
       'header.part.html',
       're.part.html',
+      'footer.part.html'
+    ]
+  },
+  '/window': {
+    title: 'Window Open Script - Web Tool',
+    keywords: '開新視窗, window.open, 網頁, 工具, 程式, 開發, Web, Tool, Program, Develop',
+    body: [
+      'header.part.html',
+      'window.part.html',
       'footer.part.html'
     ]
   },
@@ -19,7 +105,7 @@ module.exports = {
     author: 'RZ Fang',
     favicon: '',
     css: ['/resource/css.css'],
-    js: [],
+    js: ['/resource/z.min.js'],
     body: []
   }
 }
